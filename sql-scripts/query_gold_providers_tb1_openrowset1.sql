fe9a9993c99abf5793dd49e606e0e0ad7f1938a3
PRANEETH CHANDRA BUDALA <praneethchandrabudala@gmail.com>
2025-04-26 02:31:25 -0500

    Add files via upload

---
+++ sql-scripts/query_gold_providers_tb1_openrowset1.sql
@@ -0,0 +1,8 @@
+-- This is auto-generated code
+SELECT
+    TOP 100 *
+FROM
+    OPENROWSET(
+        BULK 'https://healthcaredatalakep.dfs.core.windows.net/gold/gold_providers_tb1/',
+        FORMAT = 'DELTA'
+    ) AS [result]
